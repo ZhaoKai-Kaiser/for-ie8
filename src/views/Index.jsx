5c1941f1817102f2d1b45abc0932975099a4bfb7
kaiser <765934690@qq.com>
2020-04-28 15:37:19 +0800

    first

---
+++ src/views/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Index = (props) => (
+  <div className="content">
+    <p>这里是首页</p>
+    <img src="https://digital-flowers.github.io/public/images/demo.jpg" />
+  </div>
+);
+
+export default Index;
---
+++ webpack.config.dev.js
@@ -0,0 +1,66 @@
+/*
+ * @Author: Kaiser
+ * @Date: 2020-04-27 14:44:46
+ * @Last Modified by: Kaiser
+ * @Last Modified time: 2020-04-27 15:04:44
+ * @Description: 
+ */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const es3ifyPlugin = require('es3ify-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    polyfill: './node_modules/babel-polyfill/lib/index.js', 
+    bundle: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    hot: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader', include:path.resolve(__dirname, 'src')},
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 1,
+          name: 'images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.resolve(__dirname, './src')],
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+    new es3ifyPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
