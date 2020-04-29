bc18fc99f8d0b9446d8ccf79f8fd012e09a6792c
kaiser <765934690@qq.com>
2020-04-29 11:41:21 +0800

    fix:clean-webpack-plugin

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -2,7 +2,7 @@
  * @Author: Kaiser
  * @Date: 2020-04-28 15:28:41
  * @Last Modified by: Kaiser
- * @Last Modified time: 2020-04-29 11:11:51
+ * @Last Modified time: 2020-04-29 11:39:19
  * @Description:
  */
 const webpack = require('webpack');
@@ -17,7 +17,13 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') },
     }),
-    new CleanWebpackPlugin(['../dist'], { root: __dirname }),
+    // A webpack plugin to remove/clean your build folder(s) before building
+    new CleanWebpackPlugin([utils.resolve('dist')], {
+      // Default: root of your package
+      root: utils.resolve('/'),
+      // Write logs to console.
+      verbose: true,
+    }),
     new CopyWebpackPlugin([
       {
         from: utils.resolve('public'),
